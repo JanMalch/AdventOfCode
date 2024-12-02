5742ceae11674c10f15b6498f88df534ddee652b
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-12-02 06:11:59 +0100

    Solve 2024 Day 2 Part 1

--- src/y2024/d02/Part1.kt
+++ src/y2024/d02/Part1.kt
@@ -2,9 +2,26 @@ package y2024.d02
 
 import inputLines
 import println
+import kotlin.math.absoluteValue
+
+fun List<Int>.isSafe(): Boolean {
+    if (this != sorted() && this != sortedDescending()) {
+        return false
+    }
+    var prev: Int = first()
+    for (el in this.drop(1)) {
+        val diff = el - prev
+        if (diff.absoluteValue < 1 || diff.absoluteValue > 3) {
+            return false
+        }
+        prev = el
+    }
+    return true
+}
 
 fun part1() {
     val lines = inputLines("y2024/d02/input")
-    TODO()
+    val reports = lines.map { line -> line.split(" ").map { it.toInt() } }
+    reports.count { it.isSafe() }.println("Solution")
 }
 
