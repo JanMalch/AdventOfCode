9191bc16bf68e119f2beaa028f804b4f5c9d5063
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-12-03 06:16:01 +0100

    Solve 2024 Day 3 Part 2

--- src/y2024/d03/Part2.kt
+++ src/y2024/d03/Part2.kt
@@ -5,6 +5,24 @@ import println
 
 fun part2() {
     val lines = inputLines("y2024/d03/input")
-    TODO()
+    val results = mutableListOf<Int>()
+    var enabled = true
+    for (line in lines) {
+        for (i in 0..<line.length) {
+            val subject = line.substring(i)
+            // TODO: skip over
+            if (subject.startsWith("do()")) {
+                enabled = true
+            } else if (subject.startsWith("don't()")) {
+                enabled = false
+            } else if (enabled) {
+                val match = Regex("^mul\\((\\d{1,3}),(\\d{1,3})\\)").find(subject)
+                if (match != null) {
+                    results += match.groupValues[1].toInt() * match.groupValues[2].toInt()
+                }
+            }
+        }
+    }
+    results.sum().println("Solution")
 }
 
