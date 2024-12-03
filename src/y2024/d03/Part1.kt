5d51a86f8109d617b2d1efe3c9424b1b5af29c62
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-12-03 06:08:15 +0100

    Solve 2024 Day 3 Part 1

--- src/y2024/d03/Part1.kt
+++ src/y2024/d03/Part1.kt
@@ -3,8 +3,15 @@ package y2024.d03
 import inputLines
 import println
 
+fun validMultResults(line: String): Sequence<Int> {
+    return Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)").findAll(line)
+        .map { match ->
+            match.groupValues[1].toInt() * match.groupValues[2].toInt()
+        }
+}
+
 fun part1() {
     val lines = inputLines("y2024/d03/input")
-    TODO()
+    lines.sumOf { validMultResults(it).sum() }.println("Solution")
 }
 
