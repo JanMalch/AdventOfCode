af582b388fc0d4004774d149fcd49e3f1bd505fe
JanMalch <25508038+JanMalch@users.noreply.github.com>
2024-12-05 06:31:43 +0100

    Solve 2024 Day 5 Part 1

--- src/y2024/d05/Part1.kt
+++ src/y2024/d05/Part1.kt
@@ -3,8 +3,75 @@ package y2024.d05
 import inputLines
 import println
 
+data class Rule(
+    val page: Int,
+    val beforePage: Int
+) {
+    override fun toString(): String {
+        return "$page|$beforePage"
+    }
+
+    companion object {
+        fun valueOf(value: String): Rule {
+            val (a, b) = value.split('|')
+            return Rule(
+                page = a.trim().toInt(),
+                beforePage = b.trim().toInt(),
+            )
+        }
+    }
+}
+
+data class Update(
+    val pages: List<Int>
+) {
+    val middle: Int = pages[pages.lastIndex / 2]
+
+    override fun toString(): String {
+        return pages.joinToString(separator = ",")
+    }
+
+    companion object {
+        fun valueOf(value: String): Update {
+            return Update(value.split(',').map { it.toInt() })
+        }
+    }
+}
+
+data class Rules(
+    private val rules: List<Rule>
+) {
+    private val afters: Map<Int, List<Int>> =
+        rules.groupBy(keySelector = { it.page }, valueTransform = { it.beforePage })
+
+    fun validate(page: Int, isAfter: List<Int>): Boolean {
+        if (isAfter.isEmpty()) return true
+
+        for (afterPage in isAfter) {
+            val aftersOfAfterPage = afters[afterPage].orEmpty()
+            if (page !in aftersOfAfterPage) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+fun Update.isValid(rules: Rules): Boolean {
+    for (index in pages.indices) {
+        val page = pages[index]
+        val after = pages.take(index)
+        if (!rules.validate(page, isAfter = after)) {
+            return false
+        }
+    }
+    return true
+}
+
 fun part1() {
     val lines = inputLines("y2024/d05/input")
-    TODO()
+    val rules = Rules(lines.takeWhile { '|' in it }.map { Rule.valueOf(it) })
+    val updates = lines.dropWhile { ',' !in it }.map { Update.valueOf(it) }
+    updates.filter { it.isValid(rules) }.sumOf { it.middle }.println("Solution")
 }
 
